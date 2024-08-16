103d154d5d60e1dadc9b0a61095047f0d3f0162f
menghan.sinbasara__dcar <menghan.sinbasara@dcarlife.com>
2024-08-16 16:22:16 +0800

    fix: temp

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	lep_user "bupt/lep_user/kitex_gen/lep_user/lepuser"
 	"log"
+
+	"github.com/zhongershashen/lep_lib/dal"
 )
 
 func main() {
 	svr := lep_user.NewServer(new(LepUserImpl))
-
+	dal.Init()
 	err := svr.Run()
 
 	if err != nil {
